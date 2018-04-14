fcd5738a814bfd42d6ae13dbcf76132664d63d72
Unknown <gilesjeremydev@gmail.com>
2018-04-14 14:34:07 +0200

    Add dagger component & Application class

--- app/src/main/java/com/project/jeremyg/mysimpleatlas/MySimpleAtlasApp.kt
+++ app/src/main/java/com/project/jeremyg/mysimpleatlas/MySimpleAtlasApp.kt
@@ -1,5 +1,12 @@
 package com.project.jeremyg.mysimpleatlas
 
+import com.project.jeremyg.mysimpleatlas.di.components.DaggerAppComponent
+import dagger.android.support.DaggerApplication
+import dagger.android.AndroidInjector
 
-class MySimpleAtlasApp {
+class MySimpleAtlasApp : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().application(this).build()
+    }
 }
---
+++ app/src/main/java/com/project/jeremyg/mysimpleatlas/di/components/AppComponent.kt
@@ -0,0 +1,28 @@
+package com.project.jeremyg.mysimpleatlas.di.components
+
+import android.app.Application
+import com.project.jeremyg.mysimpleatlas.di.modules.AppModule
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import dagger.android.support.DaggerApplication
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(modules = arrayOf(AndroidSupportInjectionModule::class,
+                        AppModule::class))
+interface AppComponent : AndroidInjector<DaggerApplication> {
+
+    override fun inject(instance: DaggerApplication)
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): AppComponent.Builder
+
+        fun build(): AppComponent
+    }
+}

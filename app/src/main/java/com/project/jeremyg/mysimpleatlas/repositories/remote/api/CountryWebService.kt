a9001f9ec79cc3439c2ebce3ae1fee2eec0e3e05
Unknown <gilesjeremydev@gmail.com>
2018-04-14 14:05:05 +0200

    Country web service end-point

---
+++ app/src/main/java/com/project/jeremyg/mysimpleatlas/repositories/remote/api/CountryWebService.kt
@@ -0,0 +1,16 @@
+package com.project.jeremyg.mysimpleatlas.repositories.remote.api
+
+import android.arch.lifecycle.LiveData
+import com.project.jeremyg.mysimpleatlas.repositories.models.Country
+import retrofit2.http.GET
+
+
+interface CountryWebService {
+
+    /**
+        https://api.myjson.com/bins/w0lnz
+     */
+    @GET("https://api.myjson.com/bins/w0lnz")
+    fun getCountries(): LiveData<Country>
+
+}

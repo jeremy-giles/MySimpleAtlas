4bf79b6412d84387e43f2629b3ca70e76c588262
Unknown <gilesjeremydev@gmail.com>
2018-04-14 13:53:48 +0200

    Country data class

---
+++ app/src/main/java/com/project/jeremyg/mysimpleatlas/repositories/models/Country.kt
@@ -0,0 +1,9 @@
+package com.project.jeremyg.mysimpleatlas.repositories.models
+
+
+data class Country (
+    var name: String,
+    var thumbnails: String?,
+    var photo: String?,
+    var descrption: String?
+)
